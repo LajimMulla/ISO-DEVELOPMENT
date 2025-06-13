a6274d759d312143e2af3fddb4361ed5b2faeb66
LajimMulla <121716133+LajimMulla@users.noreply.github.com>
2025-06-13 11:19:54 +0530

    Add files via upload

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Lajim Mulla on 25/03/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
